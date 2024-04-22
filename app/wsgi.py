6c476bc2de3dca25f9a7528c40bcdff05049aa70
kuria <kurialancelot@gmail.com>
2024-04-22 12:29:58 +0300

    first commit

---
+++ app/wsgi.py
@@ -0,0 +1,11 @@
+from flask import Flask 
+import socket
+
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    return f"hello from {socket.gethostname()}!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
