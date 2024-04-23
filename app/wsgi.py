8d843d10d559ebf2cee993a9f0270ce3018d5ec3
CynthiaKiconco <cynthia.kiconco@strathmore.edu>
2024-04-23 14:14:58 +0300

    containers

--- app/wsgi.py
+++ app/wsgi.py
@@ -3,9 +3,12 @@ import socket
 
 app=Flask(__name__)
 
-@app.route('/')
+@app.route('/home')
 def home():
     return f"hello from {socket.gethostname()}!"
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
