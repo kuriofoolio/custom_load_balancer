d4426e59ad80dd6c7655f050268dabde9a8dc0a6
kuriofoolio <kurialancelot@gmail.com>
2024-04-26 14:02:41 +0300

    added /heartbeat and saved thunder routes

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,11 +1,21 @@
-from flask import Flask 
+from flask import Flask , jsonify
 import socket
 
 app=Flask(__name__)
 
-@app.route('/')
+@app.route('/home', methods=['GET'])
 def home():
-    return f"hello from {socket.gethostname()}!"
+    response_data = {'message': f'Hello from Server {socket.gethostname()}', 'status code':200}
+    return jsonify(response_data)
+    # return f"Hello from Server {socket.gethostname()}"
+
+@app.route('/heartbeat', methods=['GET'])
+def heartbeat():
+    response_data = {'message': 'Server is up and running', 'status code':200}
+    return jsonify(response_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+    
