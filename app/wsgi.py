0c2874d1a57b181bf118f8e22288dd91776b2b6f
CynthiaKiconco <cynthia.kiconco@strathmore.edu>
2024-05-27 21:15:02 +0300

    Consistent Hash Map

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,7 +1,15 @@
 from flask import Flask 
 import socket
+from consistent_hashmap import ConsistentHashMap
 
 app=Flask(__name__)
+
+consistent_hashmap = ConsistentHashMap(512)
+
+servers = ['server1', 'server2', 'server3']
+for server in servers:
+
+    consistent_hashmap.add_server(server)
 
 @app.route('/home')
 def home():
