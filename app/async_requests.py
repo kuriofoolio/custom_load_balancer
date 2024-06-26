e99326efd6ac87928c75c755694b6e9794a8a033
kuria <kurialancelot@gmail.com>
2024-06-26 17:57:23 +0300

    modified a file

--- app/async_requests.py
+++ app/async_requests.py
@@ -1,14 +1,20 @@
 import asyncio
 import aiohttp
+import random
 import matplotlib.pyplot as plt
 
 # List of replica URLs
 replicas = [
-    "http://localhost:50975/home",  
-    "http://localhost:50976/home",
-    "http://localhost:50977/home"
-
+    "http://localhost:51546/home",  
+    "http://localhost:51547/home",
+    "http://localhost:51548/home",
+    "http://localhost:51549/home",
+    "http://localhost:51550/home",
+    "http://localhost:51551/home"
 ]
+
+# Weights for each replica
+weights = [0.30, 0.15, 0.21,0.10,0.20,0.04]
 
 async def fetch(session, url, counter):
     async with session.get(url) as response:
@@ -20,8 +26,8 @@ async def main():
     counter = {url: 0 for url in replicas}
     async with aiohttp.ClientSession() as session:
         for i in range(10000):
-            # Distribute requests among replicas
-            replica = replicas[i % len(replicas)]
+            # Randomly distribute requests among replicas with given weights
+            replica = random.choices(replicas, weights=weights, k=1)[0]
             task = asyncio.create_task(fetch(session, replica, counter))
             tasks.append(task)
         
@@ -34,12 +40,12 @@ if __name__ == "__main__":
     result = asyncio.run(main())
     print(result)
 
-    # Plotting the results
-    servers = list(result.keys())
-    counts = list(result.values())
+    # # Plotting the results
+    # servers = list(result.keys())
+    # counts = list(result.values())
 
-    plt.bar(servers, counts, color=['blue', 'green', 'red'])
-    plt.xlabel('Server Instances')
-    plt.ylabel('Number of Requests Handled')
-    plt.title('Requests Handled by Each Server Instance')
-    plt.show()
+    # plt.bar(servers, counts, color=['blue', 'green'])
+    # plt.xlabel('Server Instances')
+    # plt.ylabel('Number of Requests Handled')
+    # plt.title('Requests Handled by Each Server Instance')
+    # plt.show()
